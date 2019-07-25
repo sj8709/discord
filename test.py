e341fdac8ff61e1a2943cbda96253b182b3878f2
sj8709 <top5541@naver.com>
2019-07-25 21:14:51 +0900

    init

---
+++ test.py
@@ -0,0 +1,38 @@
+import asyncio
+import discord
+import urllib
+import bs4
+from urllib.request import urlopen, Request
+import re
+'''
+a = input()
+temp = a.split(" ")
+location = temp[1]
+enc_location = urllib.parse.quote(location + '+날씨')
+url = 'https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=' + enc_location #가져올 url 주소
+
+req = Request(url)
+page = urlopen(req)
+html = page.read()
+soup = bs4.BeautifulSoup(html, 'html5lib')
+
+print('{0}, {1}'.format(location, (soup.find('p', class_='cast_txt').text)))
+print('{0}, {1}'.format(location, (soup.find('p', class_='info_temperature').find('span', class_='todaytemp').text)))
+print('{0}, {1}'.format(location, (soup.find('p', class_='info_temperature').find('span', class_='todaytemp').text)))
+'''
+
+name = input()
+enc_location = urllib.parse.quote(name)
+url = 'https://www.op.gg/summoner/userName=' + enc_location #가져올 url 주소
+
+req = Request(url)
+page = urlopen(req)
+html = page.read()
+soup = bs4.BeautifulSoup(html, 'html5lib')
+
+
+print('{0}님의'.format(name))
+print('티어는 {0}'.format(soup.find('div', class_='TierRank').text))
+lp = soup.find('span', class_='LeaguePoints').text
+lp = lp.strip()
+print('{0}'.format(lp))
--- testBot.py
+++ testBot.py
@@ -25,6 +25,7 @@ async def on_message(message):
     id = message.author.id #id라는 변수에는 메시지를 보낸사람의 ID를 담습니다.
     channel = message.channel #channel이라는 변수에는 메시지를 받은 채널의 ID를 담습니다.
 
+    
     if message.content.startswith('!커맨드'): #만약 해당 메시지가 '!커맨드' 로 시작하는 경우에는
         await channel.send('커맨드') #봇은 해당 채널에 '커맨드' 라고 말합니다.
     if message.content.startswith('!hello'):
@@ -42,6 +43,38 @@ async def on_message(message):
         page = urlopen(req)
         html = page.read()
         soup = bs4.BeautifulSoup(html, 'html5lib')
-        await channel.send('현재 {0} 날씨는 {1}도 입니다.'.format(location, (soup.find('p', class_='info_temperature').find('span', class_='todaytemp').text)))
-        
+        await channel.send('현재 {0} 날씨는 {1}도 이고,'.format(location, (soup.find('p', class_='info_temperature').find('span', class_='todaytemp').text)))
+        await channel.send('{0}'.format(soup.find('p', class_='cast_txt').text))
+
+    if message.content.startswith('!롤'):
+        temp = message.content.split(" ")
+        name = temp[1]
+        enc_location = urllib.parse.quote(name)
+        url = 'https://www.op.gg/summoner/userName=' + enc_location
+        req = Request(url)
+        page = urlopen(req)
+        html = page.read()
+        soup = bs4.BeautifulSoup(html, 'html5lib')
+
+        TierRank = soup.find('div', class_='TierRank').text
+        lp = soup.find('span', class_='LeaguePoints').text
+        lp = lp.strip()
+
+        await channel.send('{0}\n 티어 : {1}\n LP : {2}'.format(name, TierRank, lp))
+
+    if message.content.startswith('!도움말'):
+        temp = message.content.split(" ")
+        try:
+            if temp[1] is not None:
+                if temp[1] == '날씨':
+                    await channel.send('!날씨 [지역]을 입력하면 현재 그 지역의 날씨를 알려줍니다.')
+                elif temp[1] == '롤':
+                    await channel.send('!롤 [닉네임]을 입력하면 해당 닉네임의 티어랭크와 리그포인트를 알려줍니다.')
+
+        except(IndexError, ValueError):
+            await channel.send('현재 사용 가능한 명령어는\n'
+                               +'!날씨\n'
+                               +'!롤\n'
+                               +'입니다.')
+
 client.run(token)
